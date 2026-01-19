6c07d35dc97ccc47b797c5d4cf65f6fed2e2023d
Satria <gegedug438@gmail.com>
2026-01-19 14:45:57 +0700

    inherit finish

--- index.php
+++ index.php
@@ -7,37 +7,52 @@ class Produk {
     $penerbit, 
     $harga,
     $jmlHalaman,
-    $waktuTamat,
-    $tipe;
+    $waktuTamat;
 
-    public function __construct($judul = "Belum diisi", $penulis = "Belum diisi", $penerbit = "Belum diisi", $harga = 0, $jmlHalaman = "Belum diisi", $waktuTamat = "Belum diisi", $tipe = "Tidak Diketahui") {
+    public function __construct($judul = "Belum diisi", $penulis = "Belum diisi", $penerbit = "Belum diisi", $harga = 0, $jmlHalaman = "Belum diisi", $waktuTamat = "Belum diisi") {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
         $this->jmlHalaman = $jmlHalaman;
         $this->waktuTamat = $waktuTamat;
-        $this->tipe = $tipe;
     }
 
     public function getLabel() {
         return "$this->penulis, $this->penerbit";
     }
-}
 
-class CetakanInfoProduk {
-    public function print(Produk $x) {
-        $text = "{$x->judul} | {$x->penulis}, diterbitkan oleh {$x->penerbit} mulai (Rp. {$x->harga })";
-        if ($x->tipe == "Buku" | $x->tipe == "BUKU" | $x->tipe == "buku") {
-            $text .= " - {$x->jmlHalaman} Halaman";
-        } else {
-            $text .= " ~ {$x->waktuTamat} Jam";
-        }
+    public function getInfoProduk() {
+        $text = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
         return $text;
     }
 }
-// judul, penulis, penerbit, harga, hal, jam, tipe
-$produk1 = new Produk("test_judul", "test_penulis", "test_penerbit", "test_harga", 100, 0, "buku");
 
-$infoproduk1 = new CetakanInfoProduk(); 
-echo $infoproduk1->print($produk1);
+class Komik extends Produk {
+    public function getInfoKomik() {
+        $text = "Komik : {$this->getInfoProduk()} - {$this->jmlHalaman} Halaman.";
+        return $text;
+    }
+}
+
+class Game extends Produk {
+    public function getInfoGame() {
+        $text = "Game : {$this->getInfoProduk()} ~ {$this->waktuTamat } Jam.";
+        return $text;
+    }
+}
+// class CetakanInfoProduk {
+//     public function print(Produk $x) {
+//         $text = "{$x->judul} | {$x->penulis}, diterbitkan oleh {$x->penerbit} mulai (Rp. {$x->harga })";
+//     }
+// }
+
+
+
+// judul, penulis, penerbit, harga, hal, jam, tipe
+$produk2 = new Game("test_game", "test_dev", "test_launcher", "test_harga", 0, 55, "game");
+$produk1 = new Komik("test_judul", "test_penulis", "test_penerbit", "test_harga", 100, 55, "Komik");
+
+echo $produk1->getInfoKomik();
+echo "<br>";
+echo $produk2->getInfoGame();
