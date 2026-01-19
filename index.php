d01bb89bc1bedb354e2cb0d0227db73dca1a6668
Satria <gegedug438@gmail.com>
2026-01-19 13:19:16 +0700

    if else

--- index.php
+++ index.php
@@ -5,13 +5,19 @@ class Produk {
     $judul,
     $penulis, 
     $penerbit, 
-    $harga;
+    $harga,
+    $jmlHalaman,
+    $waktuTamat,
+    $tipe;
 
-    public function __construct($judul = "Belum diisi", $penulis = "Belum diisi", $penerbit = "Belum diisi", $harga = 0) {
+    public function __construct($judul = "Belum diisi", $penulis = "Belum diisi", $penerbit = "Belum diisi", $harga = 0, $jmlHalaman = "Belum diisi", $waktuTamat = "Belum diisi", $tipe = "Tidak Diketahui") {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuTamat = $waktuTamat;
+        $this->tipe = $tipe;
     }
 
     public function getLabel() {
@@ -21,12 +27,17 @@ class Produk {
 
 class CetakanInfoProduk {
     public function print(Produk $x) {
-        $text = "{$x->judul} | {$x->penulis}, diterbitkan oleh {$x->penerbit} mulai (Rp. {$x->harga })"; 
+        $text = "{$x->judul} | {$x->penulis}, diterbitkan oleh {$x->penerbit} mulai (Rp. {$x->harga })";
+        if ($x->tipe == "Buku" | $x->tipe == "BUKU" | $x->tipe == "buku") {
+            $text .= " - {$x->jmlHalaman} Halaman";
+        } else {
+            $text .= " ~ {$x->waktuTamat} Jam";
+        }
         return $text;
     }
 }
-// judul, penulis, penerbit, harga
-$produk1 = new Produk("test_judul", "test_penulis", "test_penerbit", "test_harga");
+// judul, penulis, penerbit, harga, hal, jam, tipe
+$produk1 = new Produk("test_judul", "test_penulis", "test_penerbit", "test_harga", 100, 0, "buku");
 
 $infoproduk1 = new CetakanInfoProduk(); 
 echo $infoproduk1->print($produk1);
